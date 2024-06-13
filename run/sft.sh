ae73c3723d852b206ee12d95332eaa7a5381ed9f
Nguyên Nguyễn Phước <npn@NguyensMacAir.local>
2024-06-13 22:53:01 +0700

    update code

---
+++ run/sft.sh
@@ -0,0 +1,37 @@
+python src/sft.py \
+--seed=42 \
+--model_name_or_path="Qwen/Qwen2-1.5B" \
+--load_in_4bit \
+--bnb_4bit_quant_type=nf4 \
+--use_bnb_nested_quant=false \
+--use_peft \
+--lora_r=64 \
+--lora_alpha=16 \
+--lora_target_modules q_proj k_proj v_proj o_proj up_proj down_proj gate_proj \
+--dataset_name='iamnguyen/athena-ultrachat' \
+--dataset_train_split='train_sft' \
+--dataset_test_split='test_sft' \
+--report_to=none \
+--num_train_epochs=1 \
+--max_seq_length=2048 \
+--per_device_train_batch_size=2 \
+--gradient_accumulation_steps=32 \
+--learning_rate=2e-4 \
+--lr_scheduler_type='cosine' \
+--optim='paged_adamw_8bit' \
+--warmup_ratio=0.01 \
+--per_device_eval_batch_size=2 \
+--output_dir="output" \
+--logging_strategy='steps' \
+--logging_steps=1 \
+--max_steps=-1 \
+--save_strategy='steps' \
+--save_total_limit=1 \
+--save_steps=4 \
+--push_to_hub \
+--hub_strategy='checkpoint' \
+--hub_model_id='iamnguyen/ares' \
+--gradient_checkpointing 
+
+
+
---
+++ src/sft.py
@@ -0,0 +1,61 @@
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from datasets import load_dataset
+from trl import (
+    TrlParser, 
+    SFTScriptArguments, 
+    SFTConfig, 
+    SFTTrainer,
+    ModelConfig, 
+    get_quantization_config, 
+    get_kbit_device_map, 
+    get_peft_config
+)
+
+
+def main():
+    parser = TrlParser((SFTScriptArguments, SFTConfig, ModelConfig))
+    args, training_args, model_config = parser.parse_args_and_config()
+
+    torch_dtype = (
+        model_config.torch_dtype
+        if model_config.torch_dtype in ["auto", None]
+        else getattr(torch, model_config.torch_dtype)
+    )
+
+    quantization_config = get_quantization_config(model_config)
+    model_kwargs = dict(
+        revision=model_config.model_revision,
+        trust_remote_code=model_config.trust_remote_code,
+        attn_implementation=model_config.attn_implementation,
+        torch_dtype=torch_dtype,
+        use_cache=False if training_args.gradient_checkpointing else True,
+        device_map=get_kbit_device_map() if quantization_config is not None else None,
+        quantization_config=quantization_config,
+    )
+    tokenizer = AutoTokenizer.from_pretrained(model_config.model_name_or_path, use_fast=True)
+    tokenizer.pad_token = tokenizer.eos_token
+
+    raw_datasets = load_dataset(args.dataset_name)
+
+    train_dataset = raw_datasets[args.dataset_train_split]
+    eval_dataset = raw_datasets[args.dataset_test_split]
+
+    trainer = SFTTrainer(
+        model=model_config.model_name_or_path,
+        model_init_kwargs=model_kwargs,
+        args=training_args,
+        train_dataset=train_dataset,
+        eval_dataset=eval_dataset,
+        tokenizer=tokenizer,
+        peft_config=get_peft_config(model_config),
+    )
+
+    # trainer.train()
+
+if __name__=='__main__':
+    main()
